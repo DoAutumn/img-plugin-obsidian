298774f188a1ed641bfe8d17182aa23dd63ccc24
井方 <doautumn@163.com>
2024-10-10 15:52:44 +0800

    使用Gitee作为图床，插件功能开发完成

--- main.ts
+++ main.ts
@@ -1,134 +1,226 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import axios from 'axios';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+  repo: string;
+  branch: string;
+  path: string;
+  token: string;
+  subPathable: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  repo: '',
+  branch: 'master',
+  path: '/',
+  token: '',
+  subPathable: true,
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.registerEvent(
+      this.app.workspace.on('editor-paste', (event, editor, content) => {
+        this.uploadFiles(event.clipboardData?.files, event, editor);
+      })
+    );
+    this.registerEvent(
+      this.app.workspace.on('editor-drop', (event, editor, content) => {
+        this.uploadFiles(event.dataTransfer?.files, event, editor);
+      })
+    );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // This adds a settings tab so the user can configure various aspects of the plugin
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  uploadFiles(files: FileList | undefined, event: ClipboardEvent | DragEvent, editor: Editor) {
+    if (!files?.length) return;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    event.preventDefault();
+    event.stopPropagation();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    if (this.settings.subPathable) {
+      new SampleModal(this.app, files, editor, this.settings).open();
+    }
+    else {
+      uploadToGitee(files, this.settings).then((res) => {
+        editor.replaceSelection(res.filter(x => x).join('\n'));
+      })
+    }
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  onunload() {
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  }
 
-	onunload() {
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+function uploadToGitee(fileList: FileList, settings: MyPluginSettings, filePath: string = '') {
+  const tooLargeFiles = Array.from(fileList).filter(file => file.size > 1024 * 1024 * 10);
+  if (tooLargeFiles.length) {
+    new Notice(`文件大小不能超过10MB，${tooLargeFiles.map(file => file.name).join('、')}将会被丢弃`, 5000);
+  }
+  return Promise.all(Array.from(fileList).filter(file => file.size <= 1024 * 1024 * 10).map(file => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = async () => {
+        axios.post(`https://gitee.com/api/v5/repos/${settings.repo}/contents/${settings.path}/${filePath}/${file.name}`, {
+          access_token: settings.token,
+          content: (reader.result as string).replace(`data:${file.type};base64,`, ''),
+          message: 'upload image'
+        })
+          .then(res => {
+            resolve(file.type.includes('image') ? `![${file.name}](${res.data.content.download_url})` : res.data.content.download_url)
+          })
+          .catch(err => {
+            new Notice(`上传文件${file.name}失败，失败原因：${err.response.data.message}`, 5000);
+            resolve('')
+          })
+      };
+      reader.onerror = err => {
+        new Notice(`文件读取${file.name}失败`, 5000);
+        resolve('')
+      };
+      reader.readAsDataURL(file);
+    })
+  }))
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  filePath: string;
+  fileList: FileList;
+  editor: Editor;
+  settings: MyPluginSettings;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  constructor(app: App, fileList: FileList, editor: Editor, settings: MyPluginSettings) {
+    super(app);
+    this.filePath = localStorage.getItem('filePath') || '';
+    this.fileList = fileList;
+    this.editor = editor;
+    this.settings = settings;
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+
+    // 添加标题
+    contentEl.createEl('h3', { text: '请输入文件路径' });
+
+    // 创建文本输入框
+    new Setting(contentEl)
+      .setName('文件路径')
+      .setDesc('可在设置中关闭此功能')
+      .addText(text => text
+        .setPlaceholder('请输入文件路径')
+        .setValue(this.filePath)
+        .onChange(value => {
+          this.filePath = value;
+          localStorage.setItem('filePath', value);
+        }));
+
+    // 创建提交按钮
+    new Setting(contentEl).addButton(btn => {
+      btn.setButtonText('上传');
+      btn.onClick(async () => {
+        uploadToGitee(this.fileList, this.settings, this.filePath).then((res) => {
+          this.editor.replaceSelection(res.filter(x => x).join('\n'));
+          this.close();
+        })
+      });
+    });
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('仓库名')
+      .setDesc('格式: owner/repo')
+      .addText(text => text
+        .setPlaceholder('')
+        .setValue(this.plugin.settings.repo)
+        .onChange(async (value) => {
+          this.plugin.settings.repo = value;
+          await this.plugin.saveSettings();
+        })
+      )
+
+    new Setting(containerEl)
+      .setName('分支名')
+      .setDesc('默认: master')
+      .addText(text => text
+        .setPlaceholder('')
+        .setValue(this.plugin.settings.branch)
+        .onChange(async (value) => {
+          this.plugin.settings.branch = value;
+          await this.plugin.saveSettings();
+        })
+      )
+
+    new Setting(containerEl)
+      .setName('存储路径')
+      .setDesc('默认: /')
+      .addText(text => text
+        .setPlaceholder('')
+        .setValue(this.plugin.settings.path)
+        .onChange(async (value) => {
+          this.plugin.settings.path = value;
+          await this.plugin.saveSettings();
+        })
+      )
+
+    new Setting(containerEl)
+      .setName('私人令牌')
+      .setDesc('获取方式：头像 -> 设置 -> 私人令牌')
+      .addText(text => text
+        .setPlaceholder('')
+        .setValue(this.plugin.settings.token)
+        .onChange(async (value) => {
+          this.plugin.settings.token = value;
+          await this.plugin.saveSettings();
+        })
+      )
+
+    new Setting(containerEl)
+      .setName('文件路径')
+      .setDesc('粘贴文件时是否需要输入文件路径，文件路径始终为存储路径下的子路径')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.subPathable)
+        .onChange(async (value) => {
+          this.plugin.settings.subPathable = value;
+          await this.plugin.saveSettings();
+        })
+      )
+  }
 }
